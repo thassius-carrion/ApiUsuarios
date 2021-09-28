597a285e5f91fe0c4d8b4621f2cec687a3d03548
Thassius Carrion Mendes de Oliveira <thassius.oliveira@programmers.com.br>
2021-09-28 15:02:58 -0300

    Creating Login operations

---
+++ PrimeiraAPI/Dtos/ErroRespostaDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeiraAPI.Dtos
+{
+    public class ErroRespostaDto
+    {
+        public int Status { get; set; }
+        public string Erro { get; set; }
+
+    }
+}
---
+++ PrimeiraAPI/Dtos/LoginRespostaDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeiraAPI.Dtos
+{
+    public class LoginRespostaDto
+    {
+        public string Email { get; set; }
+        public string Nome { get; set; }
+        public string Token { get; set; }
+
+    }
+}
