9f2384793dfea18200943fa8f6bd3948bcaefce4
Thassius Carrion Mendes de Oliveira <thassius.oliveira@programmers.com.br>
2021-09-28 07:33:48 -0300

    Creating API and entity Usuario

---
+++ PrimeiraAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeiraAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ObterUsuario()
+        {
+            var usuario = new Usuario()
+            {
+                Nome = "Teste",
+                Email = "teste@teste.com",
+                Senha = "12345ab"
+            };
+            return Ok(usuario);
+        }
+
+    }
+}
---
+++ PrimeiraAPI/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PrimeiraAPI.Models
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        [JsonIgnore]
+        public string Senha { get; set; }
+
+    }
+}
